7c685375873d434af41def176e1289745297fca6
SANTIAGO HERRERA MUNERA <santiagoherrera1141378@correo.itm.edu.co>
2025-09-10 22:45:37 -0500

    Circle and Square

---
+++ Act_2-Poo_Heritage/Poo_Heritage_Core/Circle.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Poo_Heritage_Core;
+
+public class Circle : GeometricFigure
+{
+    private double _r;
+    public double R
+    {
+        get => _r;
+        set => _r = ValidateR(value);
+    }
+    public Circle(string name, double r)
+    {
+        Name = name;
+        R = r;
+    }
+   
+    public override double GetArea() => Math.PI * Math.Pow(R, 2);
+
+    public override double GetPerimeter() => 2 * Math.PI * R;
+
+    private double ValidateR(double r)
+    {
+        if (r <= 0)
+        {
+            throw new Exception($"The radius {r} is less than or equal to zero, it is incorrect");
+        }
+        return r;
+    }
+
+}
+
---
+++ Act_2-Poo_Heritage/Poo_Heritage_Core/Square.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Poo_Heritage_Core;
+
+public class Square : GeometricFigure
+{
+    private double _a;
+    public double A
+    {
+        get => _a;
+        set => _a = ValidateR(value);
+    }
+    public Square(string name, double a)
+    {
+        Name = name;
+        A = a;
+    }
+   
+    public override double GetArea() => (A * A);
+
+    public override double GetPerimeter() => 4 * A;
+
+    private double ValidateR(double a)
+    {
+        if (a <= 0)
+        {
+            throw new Exception($"The radius {a} is less than or equal to zero, it is incorrect");
+        }
+        return a;
+    }
+
+
+}
