ccbd47300398023859cc8326c92bc6d12f3ab002
SANTIAGO HERRERA MUNERA <santiagoherrera1141378@correo.itm.edu.co>
2025-09-10 22:24:34 -0500

    Geometric Figure

---
+++ Act_2-Poo_Heritage/Poo_Heritage_Core/GeometricFigure.cs
@@ -0,0 +1,17 @@
+﻿namespace Poo_Heritage_Core;
+
+public abstract class GeometricFigure
+{
+
+    public string Name { get; set; } = null!;
+
+    public abstract double GetArea();
+    public abstract double GetPerimeter();
+
+    public override string ToString()
+    {
+        return $"{Name} => Area: {GetArea()}  Perimeter: {GetPerimeter()}";
+
+    }
+
+}
