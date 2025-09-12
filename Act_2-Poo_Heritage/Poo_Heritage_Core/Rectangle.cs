d9b49adc3c8d6ba76d8e66e56ee0be64ef7391c0
SANTIAGO HERRERA MUNERA <santiagoherrera1141378@correo.itm.edu.co>
2025-09-11 22:03:04 -0500

    Rectangle is create.

---
+++ Act_2-Poo_Heritage/Poo_Heritage_Core/Rectangle.cs
@@ -0,0 +1,32 @@
+﻿namespace Poo_Heritage_Core;
+
+public class Rectangle : Square
+{
+    private double _b;
+
+    public double B
+    {
+        get => _b;
+        set => _b = ValidateB(value);
+    }
+
+    public Rectangle(string name, double a, double b) : base(name, a)
+    {
+        Name = name;
+        A = a;
+        B = b;
+    }
+
+    public override double GetArea() => A * B;
+
+    public override double GetPerimeter() => 2 * (A + B);
+
+    private double ValidateB(double b)
+    {
+        if (b <= 0)
+        {
+            throw new Exception($"The radius {b} is less than or equal to zero, it is incorrect");
+        }
+        return b;
+    }
+}
