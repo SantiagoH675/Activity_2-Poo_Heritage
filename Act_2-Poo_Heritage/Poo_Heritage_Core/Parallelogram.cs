9385eccf3d6ea039c081b6e6e8c4b588a3711580
SANTIAGO HERRERA MUNERA <santiagoherrera1141378@correo.itm.edu.co>
2025-09-11 22:22:59 -0500

    Load is loaded and class diagram is updated

---
+++ Act_2-Poo_Heritage/Poo_Heritage_Core/Parallelogram.cs
@@ -0,0 +1,33 @@
+﻿namespace Poo_Heritage_Core;
+
+public class Parallelogram : Rectangle
+{
+    private double _h;
+
+    public double H
+    {
+        get => _h;
+        set => _h = ValidateH(value);
+    }
+
+    public Parallelogram(string name, double a, double b, double h) : base(name, a, b)
+    {
+        Name = name;
+        A = a;
+        B = b;
+        H = h;
+    }
+
+    public override double GetArea() => B * H;
+
+    public override double GetPerimeter() => 2 * (A + B);
+
+    private double ValidateH(double h)
+    {
+        if (h <= 0)
+        {
+            throw new Exception($"The radius {h} is less than or equal to zero, it is incorrect");
+        }
+        return h;
+    }
+}
